dc3503359e29a707d021dde59d9ec6899894cb8a
ChrisH81 <chrishurst1981@icloud.com>
2017-06-28 19:56:44 +0200

    Add color to evaluation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,24 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+var input = document.getElementsByTagName('input');
+
+keyupHandler = function(input){
+    if (input.value == 'red'){
+        input.setAttribute('class', 'red');
+    }
+    else if (input.value == 'yellow'){
+        input.setAttribute('class', 'yellow');
+    }
+    else {
+        input.setAttribute('class', 'green');
+    }
+}
+
+for (i=0; i<input.length; i++){
+    input[i].onkeyup = function(){
+        keyupHandler(this);
+    }
+}
