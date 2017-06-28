5d910a11f531bb1109f740501bc3053dc25de2f6
ChrisH81 <chrishurst1981@icloud.com>
2017-06-29 00:05:41 +0200

    create system for average score

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,4 +1,4 @@
 class Batch < ApplicationRecord
   has_many :students, dependent: :destroy
-  
+
 end
--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,5 +1,9 @@
 class Evaluation < ApplicationRecord
   belongs_to :student
-  
+
   default_scope { order(date: :desc) }
+
+  def self.average_score
+    average(:score)
+  end
 end
