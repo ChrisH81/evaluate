8a48d65261fedb4c1c614463fd337aa46c62be5f
ChrisH81 <chrishurst1981@icloud.com>
2017-06-28 17:36:29 +0200

    Add evalution to student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,34 +2,26 @@ class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :edit, :update, :destroy]
   before_action :set_batch
 
-  # GET /students
-  # GET /students.json
   def index
     @students = Student.all
   end
 
-  # GET /students/1
-  # GET /students/1.json
   def show
   @student = Student.find(params[:id])
   @batch = Batch.find(@student.batch_id)
   end
 
-  # GET /students/new
   def new
     @student = Student.new
   end
 
-  # GET /students/1/edit
   def edit
   end
 
-  # POST /students
-  # POST /students.json
   def create
     @student = Student.new(student_params)
     @student.batch_id = @batch.id
-
+  
     respond_to do |format|
       if @student.save
         format.html { redirect_to @student.batch, notice: 'Student was successfully created.' }
@@ -41,8 +33,6 @@ class StudentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /students/1
-  # PATCH/PUT /students/1.json
   def update
     respond_to do |format|
       if @student.update(student_params)
@@ -55,8 +45,6 @@ class StudentsController < ApplicationController
     end
   end
 
-  # DELETE /students/1
-  # DELETE /students/1.json
   def destroy
     @student.destroy
     respond_to do |format|
@@ -66,17 +54,16 @@ class StudentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_student
-      @student = Student.find(params[:id])
-    end
 
-    def set_batch
-      @batch = Batch.find(params[:batch_id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def student_params
-      params.require(:student).permit(:first_name, :last_name, :image, :batch_id)
-    end
+  def set_batch
+    @batch = Batch.find(params[:batch_id])
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :image, :batch_id)
+  end
 end
