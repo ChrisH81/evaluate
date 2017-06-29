0986bd75a4f46cc8fd8fb6d5d410110823fc122f
ChrisH81 <chrishurst1981@icloud.com>
2017-06-29 13:51:16 +0200

    Add average with different algo

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,6 @@ class StudentsController < ApplicationController
   def show
   @student = Student.find(params[:id])
   @batch = Batch.find(@student.batch_id)
-
   end
 
   def new
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,8 @@ class Student < ApplicationRecord
   belongs_to :batch
   has_many :evaluations, dependent: :destroy
 
+
+
   def average_score
     evaluations.average_score
   end
