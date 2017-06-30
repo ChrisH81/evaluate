e37da3bf74a1ca7778d4e2d2521d5e78096f4534
ChrisH81 <chrishurst1981@icloud.com>
2017-06-30 13:16:33 +0200

    refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-
-def product_price
-  if general.offer.blank?
-    return "product-price"
-  else
-    return "product-special-price"
-  end
-end
-
 end
--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,6 +1,5 @@
 class Batch < ApplicationRecord
   has_many :students, dependent: :destroy
-
-
   default_scope { order(start_date: :asc) }
+
 end
